a26141854d20a5ba981255351c0993329cd8e1fd
seilylook <seilylook@naver.com>
2021-08-06 12:41:07 +0900

    feature DOM ref

--- src/App.js
+++ src/App.js
@@ -1,47 +1,49 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import "./ValidationSample.css";
 
 import React, { Component } from "react";
 
-const App = () => {
-  const [username, setUsername] = useState("");
-  const [message, setMessage] = useState("");
-  const onChangeUsername = e => setUsername(e.target.value);
-  const onChangeMessage = e => setMessage(e.target.value);
-  const onClick = () => {
-    alert(username + " : " + message);
-    setUsername("");
-    setMessage("");
+class App extends Component {
+  state = {
+    password: "",
+    clicked: false,
+    validated: false
   };
 
-  const onKeyPress = e => {
-    if (e.key === "Enter") {
-      onClick();
-    }
+  handleChange = e => {
+    this.setState({
+      password: e.target.value
+    });
   };
 
-  return (
-    <div>
-      <h1>이벤트 연습</h1>
-      <input
-        type="text"
-        placeholder="사용자명"
-        value={username}
-        onChange={onChangeUsername}
-      />
+  handleButtonClick = () => {
+    this.setState({
+      clicked: true,
+      validated: this.state.password === "0000"
+    });
+  };
 
-      <input
-        type="text"
-        placeholder="message"
-        value={message}
-        onChange={onChangeMessage}
-        onKeyPress={onKeyPress}
-      ></input>
-
-      <button onClick={onClick}>확인</button>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          className={
+            this.state.clicked
+              ? this.state.validated
+                ? "success"
+                : "failure"
+              : ""
+          }
+        />
+        <button onClick={this.handleButtonClick}>검증하기</button>
+      </div>
+    );
+  }
+}
 
 export default App;
