5440de23bdd134267381a2fd0b9046bff61d7b19
seilylook <seilylook@naver.com>
2021-08-08 19:15:29 +0900

    feature hooks useState

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
-import { useState } from "react";
+import React from "react";
+import Info from "./Info";
 
-import React, { Component } from "react";
-import LifeCycleSample from "./LifeCycleSample";
-
-function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-class App extends Component {
-  state = {
-    color: "#000000"
-  };
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor()
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <LifeCycleSample color={this.state.color} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <Info />;
+};
 
 export default App;
