92b0e6bc68f77da7c4db9633f9e0e3e95f15895c
seilylook <seilylook@naver.com>
2021-08-11 11:34:25 +0900

    feature custom hooks

--- src/Info.js
+++ src/Info.js
@@ -1,29 +1,19 @@
 import React, { useReducer } from "react";
-
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value
-  };
-}
+import useInputs from "./useInputs";
 
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [value, onChange] = useInputs({
     name: "",
     nickname: ""
   });
 
   const { name, nickname } = state;
 
-  const onChange = e => {
-    dispatch(e.target);
-  };
-
   return (
     <div>
       <div>
         <input name="name" value={name} onChange={onChange} />
-        <input nickname="nickname" value={nickname} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
 
       <div>
@@ -33,7 +23,7 @@ const Info = () => {
         </div>
 
         <div>
-          <b>닉네임: </b>
+          <b>닉네임</b>
           {nickname}
         </div>
       </div>
